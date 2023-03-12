22842d49e56db0e7c7aae4387d0ccfa09bfb1cd3
Vladimir Fursov <vladimirfursov228@gmail.com>
2023-03-13 01:36:33 +0200

    fix pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+<Html lang="en" style={{ background: "linear-gradient(45deg, #e6f2ff, #ffffff)" }}>
       <Head />
       <body>
         <Main />
